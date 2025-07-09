4fe707c559ef78c7a4a43a4d24e14fea0849e861
wupeng <603928546@qq.com>
2025-07-09 23:40:58 +0800

    工程整理

---
+++ react_client/src/project/App.jsx
@@ -0,0 +1,14 @@
+import { Routes } from "react-router-dom";
+
+
+export default function App(){
+    return (
+        <div id="app" className="container">
+            <div className="content">
+                <Routes>
+                    
+                </Routes>
+            </div>
+        </div>
+    )
+}
